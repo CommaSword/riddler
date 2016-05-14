90efa76a36ea9757d04b759629ead47cefc83b7d
amir-arad <amira@wix.com>
2016-05-15 01:22:41 +0300

    configure BO port from env

--- riddler-back-office/index.js
+++ riddler-back-office/index.js
@@ -41,7 +41,7 @@ app.use(settings.httpAdminRoot,RED.httpAdmin);
 // Serve the http nodes UI from /api
 app.use(settings.httpNodeRoot,RED.httpNode);
 
-server.listen(8000);
+server.listen(process.env.node_red_port);
 
 // Start the runtime
 RED.start();
