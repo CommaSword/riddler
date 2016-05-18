6e7eb2da911be63ec1b98ba1c48369d43091ac60
Amir Arad <amira@wix.com>
2016-05-19 01:19:42 +0300

    extracted riddle logic from station FW

--- riddler-station/index.js
+++ riddler-station/index.js
@@ -5,83 +5,59 @@ require('dotenv').config();
 var five = require('johnny-five');
 var express = require('express');
 
-const board = new five.Board({
+var app = express();
+
+var port = process.env.port || 80;
+
+// most basic body parser for raw strings
+app.use(function rawBody(req, res, next) {
+	req.setEncoding('utf8');
+	req.rawBody = '';
+	req.on('data', function (chunk) {
+		req.rawBody += chunk;
+	});
+	req.on('end', function () {
+		next();
+	});
+});
+
+const boardConfig = {
 	repl:false,
 	// debug: false
-});
-var interval = 1;
-
-var state = {
-	timeout_value: 3,
-	time_since_press: 0
 };
 
-board.on("ready", function() {
-
-	var button = new five.Button(10);
-	var red = new five.Led(8);
-	var green = new five.Led(9);
-	function reset(){
-		state.time_since_press = 0;
-		console.log('Click! state: ', JSON.stringify(state));
-		calcLedsStatus();
-	}
-	button.on('press', reset);
-	button.on('hold', reset);
-
-	function calcLedsStatus(){
-		if (state.time_since_press > state.timeout_value){
-			red.on();
-			green.off();
-		} else {
-			red.off();
-			green.on();
-		}
-	}
-	calcLedsStatus();
-	setInterval(function() {
-		state.time_since_press = state.time_since_press + interval;
-		console.log('Tick! state: ', JSON.stringify(state));
-		calcLedsStatus();
-	}, interval * 1000);
-	runServer();
-});
-function runServer() {
-	const BODY = 'rawBody';
-	var app = express();
-// most basic body parser for raw strings
-	app.use(function rawBody(req, res, next) {
-		req.setEncoding('utf8');
-		req[BODY] = '';
-		req.on('data', function (chunk) {
-			req[BODY] += chunk;
+function initNextBoard() {
+	var board = new five.Board(boardConfig);
+	var id;
+	var isOk = false;
+	board.on("ready", function () {
+		id = identifyRiddle(this);
+		isOk = true;
+		console.log('ready board for riddle' + id);
+		var route = express.Router();
+		route.get('/', function (req, res) {
+			res.json(isOk);
 		});
-		req.on('end', function () {
-			next();
+		route.use(function(req, res, next) {
+			isOk? next() : res.status(500).send('Board Disconnected');
 		});
+		require('./riddles/'+id)(route, this);
+		app.use('/'+id, route);
+		console.log('started riddle' + id);
+		initNextBoard();
 	});
-
-	app.get('/data', function (req, res) {
-		res.json(state);
-	});
-
-	app.post('/timeout_value', function (req, res) {
-		console.log('set_timeout_value', req[BODY]);
-		if (req[BODY].match(/^\d+$/)) {
-			state.timeout_value = parseInt(req[BODY]);
-		}
-		res.json(state);
-	});
-
-	app.post('/time_since_press', function (req, res) {
-		console.log('set_time_since_press', req[BODY]);
-		if (req[BODY].match(/^\d+$/)) {
-			state.time_since_press = parseInt(req[BODY]);
-		}
-		res.json(state);
-	});
-
-	app.listen(process.env.riddle1_port, function () {
-		console.log('Example app listening on port', process.env.riddle1_port);
+	board.on("close", function () {
+		isOk = false;
+		console.log('board closed ' + id);
 	});
 }
+
+function identifyRiddle(board){
+	// TODO use input from board to identify hardware
+	return 'riddle1';
+}
+
+app.listen(port, function () {
+	console.log('Station API listening on port', port);
+	initNextBoard();
+});
---
+++ riddler-station/riddles/riddle1.js
@@ -0,0 +1,74 @@
+/**
+ * Created by amira on 17/5/16.
+ */
+var five = require('johnny-five');
+
+module.exports = function riddle1(api, board){
+
+	var interval = 1;
+
+	var state = {
+		timeout_value: 3,
+		time_since_press: 0
+	};
+
+	var button = new five.Button({
+		pin:10,
+		board:board
+	});
+	var red = new five.Led({
+		pin:8,
+		board:board
+	});
+	var green = new five.Led({
+		pin:9,
+		board:board
+	});
+
+	function reset(){
+		state.time_since_press = 0;
+		console.log('Click! state: ', JSON.stringify(state));
+		calcLedsStatus();
+	}
+	button.on('press', reset);
+	button.on('hold', reset);
+
+	function calcLedsStatus(){
+		if (state.time_since_press > state.timeout_value){
+			red.on();
+			green.off();
+		} else {
+			red.off();
+			green.on();
+		}
+	}
+	calcLedsStatus();
+	setInterval(function() {
+		state.time_since_press = state.time_since_press + interval;
+		console.log('Tick! state: ', JSON.stringify(state));
+		calcLedsStatus();
+	}, interval * 1000);
+
+
+	api.get('/data', function (req, res) {
+		res.json(state);
+	});
+
+	api.post('/timeout_value', function (req, res) {
+		console.log('set_timeout_value', req[BODY]);
+		if (req.rawBody.match(/^\d+$/)) {
+			state.timeout_value = parseInt(req[BODY]);
+		}
+		res.json(state);
+	});
+
+	api.post('/time_since_press', function (req, res) {
+		console.log('set_time_since_press', req[BODY]);
+		if (req.rawBody.match(/^\d+$/)) {
+			state.time_since_press = parseInt(req[BODY]);
+		}
+		res.json(state);
+	});
+
+
+};
