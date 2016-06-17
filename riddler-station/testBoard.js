8e82ee929bf494b8d92a7bf2377f37a81f0687df
amir-arad <amira@wix.com>
2016-06-17 14:37:50 +0300

    small changes

--- riddler-station/testBoard.js
+++ riddler-station/testBoard.js
@@ -1,8 +1,16 @@
 /**
  * Created by amira on 10/6/16.
  */
+var five = require('johnny-five');
 
-require('./detect-board')(function (config, raw) {
+require('./detect-board')(function (config, raw, board) {
 	console.log('\nraw value: ' + raw + '\n');
-	process.exit(0);
+	new five.Pin({
+		pin: 7,
+		mode: 0,
+		board:board
+	}).query(function(state){
+		console.log('\n\n##################value in pin is (raw:'+state.value+')\n\n');
+		process.exit(0);
+	});
 });
