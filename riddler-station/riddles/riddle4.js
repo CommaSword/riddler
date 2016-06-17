2627a9cd38fa54117325bfa7fbdd0bebd6d493d3
OmerSS <osh@consumerpysics.com>
2016-06-18 01:16:12 +0300

    Added more statuses - different for complex and simple

--- riddler-station/riddles/riddle4.js
+++ riddler-station/riddles/riddle4.js
@@ -7,14 +7,24 @@ var BLINK_INTERVAL = 500;
 
 var riddle_status = {
 	START: 'Initial state',
-	SIMPLE: 'Broken - simple mode',
-	COMPLEX: 'Broken - complex mode',
-	SUCCESS: 'Fixed'
+	SIMPLE_START: 'Simple mode - started',
+    SIMPLE_SWITCH: 'Simple mode - switch down',
+    SIMPLE_PUSH: 'Simple mode - button press',
+    SIMPLE_FINISH: 'Simple mode - button hold - ready for power up',
+
+	COMPLEX_START: 'Complex  mode - started',
+    COMPLEX_SWITCH_ONLY: 'Complex  - switch is down',
+    COMPLEX_CABLES_ONLY: 'Complex  - switch up and cables out',
+    COMPLEX_CABLES_SWITCH_INCORRECT: 'Complex  - cables out before switch down',
+    COMPLEX_CABLES_SWITCH_CORRECT: 'Complex  - switch down before cables',
+    COMPLEX_CABLES_BAD_SWITCH: 'Complex  - switch down, cables out then switch up',
+    COMPLEX_FINISH: 'Complex  - ready for power up',
 };
 
 var switch_status = {
-	ON: 'Switch is up',
-	OFF: 'Switch is down'
+	ON: 'Switch is up - Red switch == 1',
+	OFF: 'Switch is down - Red switch == 0',
+    HOLD: 'Switch is holding - Red switch == 1'
 };
 
 var led_status = {
@@ -33,11 +43,17 @@ module.exports = function riddle4(api, board){
 
 	var state = {
 		status: riddle_status.START,
-	    state_button: switch_status.ON,
+	    switch_button: switch_status.ON,
 		push_button: switch_status.OFF,
-		leds: {'GREEN': led_status.ON, 'RED': led_status.OFF, 'BLUE': led_status.OFF}
+		leds: {'GREEN': led_status.ON, 'RED': led_status.OFF, 'BLUE': led_status.OFF},
+        cables: {5,6,7,8,9,10}
 	};
 
+    // Const for riddle
+    var fourCable = {6,9};
+    var sixCable = {};
+
+    // Defining hardware
 	var leds = {};
 	leds['RED'] = new five.Led({
 		pin: 13,
@@ -60,7 +76,7 @@ module.exports = function riddle4(api, board){
 		board: board
 	});
 
-	var stateButton = new five.Button({
+	var switchButton = new five.Button({
 		pin: 4,
 		board: board
 	});
@@ -69,6 +85,51 @@ module.exports = function riddle4(api, board){
 		pin: 11,
 		board: board
 	});
+
+    // pins for cables
+    var pinsState = {
+        5:1,
+        6:1,
+        7:1,
+        8:1,
+        9:1,
+        10:1,
+    };
+    var pins = [];
+    for (var i = 0; i < 6; i++) {
+        pins.push(new five.Pin({
+            pin: i + 5,
+            mode: 0
+        }));
+        pins[i].read(function(error, value){
+            calculateAll();
+        });
+    }
+
+    function checkSetPins() {
+        // Checks and sets the pins states that the cable connect to.
+        // If the cable is connected to the board then the relevant pin's pinState will be 1, else pinState = 0
+        pins.forEach(function (currentPin, pinNumber) {
+            currentPin.query(function (pinState) {
+                pinsState[pinNumber] = pinState.value;
+            });
+        });
+    }
+
+    function checkConnectedWires() {
+        // Return an array with cables that are still connected
+        checkSetPins();
+        connected = [];
+        // Checks if any of the cables have been disconnected
+        Object.keys(pinsState).forEach(function(pinId){
+            var pinVal = pinsState[pinId];
+            if (pinVal == 1){
+                connected.append(pinId)
+            }
+        });
+        return connected;
+    }
+
 
 	function setLeds(array_state) {
 		Object.keys(leds).forEach(function (led, idx){
@@ -85,76 +146,220 @@ module.exports = function riddle4(api, board){
 		})
 	}
 
-	function stateButtonPress() {
-		state.stateButton = switch_status.OFF;
-		if (state.status === riddle_status.SIMPLE) {
-			setLeds([led_status.OFF, led_status.OFF, led_status.ON])
-		} else if (state.status === riddle_status.COMPLEX) {
-			setLeds([led_status.OFF, led_status.ON, led_status.OFF])
-		} else {
-			setLeds([led_status.OFF, led_status.OFF, led_status.OFF])
-		}
+
+    //
+    // switchButton and pushButton functions
+    //
+	function switchButtonPress() {
+        // switchButton = 0, start handling the riddle
+		state.switch_button = switch_status.OFF;
+		if (state.status === riddle_status.SIMPLE_START) {
+            state.status === riddle_status.SIMPLE_SWITCH;
+            return;
+        }
+
+        if (state.status === riddle_status.COMPLEX_START) {
+            state.status === riddle_status.COMPLEX_SWITCH_ONLY;
+        } else if (state.status === riddle_status.COMPLEX_CABLES_ONLY) {
+            state.status === riddle_status.COMPLEX_CABLES_SWITCH_INCORRECT;
+        } else if (state.status === riddle_status.COMPLEX_CABLES_BAD_SWITCH) {
+            state.status === riddle_status.COMPLEX_CABLES_SWITCH_CORRECT;
+        } else if (state.status === riddle_status.START) {
+            state.status === riddle_status.COMPLEX_FINISH;
+        }
 	}
 
-	function stateButtonRelease() {
-		state.state_button = switch_status.ON;
-		if ((state.status === riddle_status.START) || (state.status === riddle_status.SUCCESS){
-			setLeds([led_status.ON, led_status.OFF, led_status.OFF])
-		}
-		else {
-			setLeds([led_status.OFF, led_status.ON, led_status.OFF])
-		}
+	function switchButtonRelease() {
+        // switchButton = 1, riddle finished or can't start
+		state.switch_button = switch_status.ON;
+        if (state.status === riddle_status.SIMPLE_SWITCH) {
+            state.status === riddle_status.SIMPLE_START;
+        }else if (state.status === riddle_status.SIMPLE_PUSH) {
+            state.status === riddle_status.SIMPLE_START;
+        }else if (state.status === riddle_status.SIMPLE_FINISH) {
+            state.status = riddle_status.START
+        }
+
+        if (state.status === riddle_status.COMPLEX_SWITCH_ONLY) {
+            state.status === riddle_status.COMPLEX_START;
+        } else if (state.status === riddle_status.COMPLEX_CABLES_SWITCH_INCORRECT) {
+            state.status === riddle_status.COMPLEX_CABLES_ONLY;
+        } else if (state.status === riddle_status.COMPLEX_CABLES_SWITCH_CORRECT) {
+            state.status === riddle_status.COMPLEX_CABLES_BAD_SWITCH;
+        } else if (state.status === riddle_status.COMPLEX_FINISH) {
+            state.status === riddle_status.START;
+        }
+	}
+
+	function pushButtonPress() {
+        // Red button is pressed - do not care if holding it or not
+        state.push_button = switch_status.ON;
+        if (state.status === riddle_status.SIMPLE_START) {
+            playBuzzer();
+        }else if (state.status === riddle_status.SIMPLE_SWITCH) {
+            state.status === riddle_status.SIMPLE_PUSH;
+        }else if (state.status === riddle_status.SIMPLE_FINISH) {
+            return;
+        }
+        if (!isRiddleSimple()) {
+            playBuzzer();
+            return;
+        }
 	}
 
 	function pushButtonHold() {
-		state.push_button = switch_status.ON;
-		if (state.status === riddle_status.SIMPLE) {
-			setLeds([led_status.OFF, led_status.OFF, led_status.BLINKING])
-		} else if (state.status === riddle_status.COMPLEX) {
-			buzzer.play({
-				tempo: 150, // Beats per minute, default 150
-				song: [ // An array of notes that comprise the tune
-					[ "c1", 1 ],
-					[ "e4", 2 ],
-					[ "g4", 3 ],
-					[ "d9", 4 ] // null indicates "no tone" for the beats indicated
-				]
-			});
-		}
+        // Red button was held for 10 whole seconds
+		state.push_button = switch_status.HOLD;
+        if (state.status === riddle_status.SIMPLE_START) {
+            playBuzzer();
+        }else if (state.status === riddle_status.SIMPLE_SWITCH) {
+            state.status === riddle_status.SIMPLE_FINISH;
+        }else if (state.status === riddle_status.SIMPLE_PUSH) {
+            state.status === riddle_status.SIMPLE_FINISH;
+        }else if (state.status === riddle_status.SIMPLE_FINISH) {
+            return;
+        }
+
+        if (!isRiddleSimple()) {
+            playBuzzer();
+            return;
+        }
 	}
 
 	function pushButtonRelease() {
+        // Red button is released
 		state.push_button = switch_status.OFF;
-		buzzer.stop()
-		if (state.status === riddle_status.SIMPLE) {
-			setLeds([led_status.OFF, led_status.OFF, led_status.ON])
-		} else if (state.status === riddle_status.COMPLEX) {
-			setLeds([led_status.OFF, led_status.ON, led_status.OFF])
-		}
+		buzzer.stop();
+        if (state.status === riddle_status.SIMPLE_START) {
+            return;
+        }else if (state.status === riddle_status.SIMPLE_SWITCH) {
+            return;
+        }else if (state.status === riddle_status.SIMPLE_PUSH) {
+            return;
+        }else if (state.status === riddle_status.SIMPLE_FINISH) {
+            return;
+        }
+
+        if (!isRiddleSimple()) {
+            playBuzzer();
+            return;
+        }
 	}
 
 
 
+    function playBuzzer(){
+        buzzer.play({
+            tempo: 150, // Beats per minute, default 150
+            song: [ // An array of notes that comprise the tune
+                [ "c1", 1 ],
+                [ "e4", 2 ],
+                [ "g4", 3 ],
+                [ "d9", 4 ] // null indicates "no tone" for the beats indicated
+            ]
+        });
+    }
 
-	var interval = 1;
-	state = {
-		pinStates : 0
-	};
+    function isRiddleSimple(){
+        // returns true if the riddle type is simple
+        if ((state.status === riddle_status.SIMPLE_FINISH) || (state.status === riddle_status.SIMPLE_PUSH) ||
+            (state.status === riddle_status.SIMPLE_START) || (state.status === riddle_status.SIMPLE_SWITCH)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
-	var pin = five.Pin({
-			pin: 9,
-			mode: 0
-		});
+    function calculateAll(){
+        // Calculate all of the states from scratch
+        var connectedWires = checkConnectedWires();
+        // Make sure all wires are connected in simple riddle
+        if ((connectedWires.length < 6) && (isRiddleSimple()) ){
+            playBuzzer();
+            return
+        }
+        buzzer.stop();
+
+        if (state.status === riddle_status.START) {
+            setLeds([led_status.OFF, led_status.OFF, led_status.OFF]);
+            setLeds([led_status.ON, led_status.OFF, led_status.OFF]);
+        }else if ((state.status === riddle_status.SIMPLE_START) || (state.status === riddle_status.SIMPLE_SWITCH)) {
+            setLeds([led_status.OFF, led_status.OFF, led_status.OFF]);
+            setLeds([led_status.OFF, led_status.ON, led_status.OFF]);
+        }else if (state.status === riddle_status.SIMPLE_PUSH) {
+            setLeds([led_status.OFF, led_status.OFF, led_status.OFF]);
+            setLeds([led_status.OFF, led_status.OFF, led_status.BLINKING]);
+        }else if (state.status === riddle_status.SIMPLE_FINISH) {
+            setLeds([led_status.OFF, led_status.OFF, led_status.OFF]);
+            setLeds([led_status.OFF, led_status.OFF, led_status.ON]);
+        }
+
+        if ((state.status === riddle_status.COMPLEX_CABLES_ONLY)||(state.status === riddle_status.COMPLEX_CABLES_SWITCH_INCORRECT)
+            || (state.status === riddle_status.COMPLEX_CABLES_BAD_SWITCH)) {
+            playBuzzer();
+            setLeds([led_status.OFF, led_status.OFF, led_status.OFF]);
+            setLeds([led_status.OFF, led_status.ON, led_status.OFF]);
+
+        } else if (state.status === riddle_status.COMPLEX_CABLES_SWITCH_CORRECT) {
+            setLeds([led_status.OFF, led_status.OFF, led_status.OFF]);
+            setLeds([led_status.OFF, led_status.OFF, led_status.BLINKING]);
+
+        } else if ((state.status === riddle_status.COMPLEX_START) || (state.status === riddle_status.COMPLEX_SWITCH_ONLY)){
+            // No buzzer
+            setLeds([led_status.OFF, led_status.OFF, led_status.OFF]);
+            setLeds([led_status.OFF, led_status.ON, led_status.OFF]);
+
+        } else if (state.status === riddle_status.COMPLEX_FINISH) {
+            setLeds([led_status.OFF, led_status.OFF, led_status.OFF]);
+            setLeds([led_status.OFF, led_status.OFF, led_status.ON]);
+        }
+
+        if ((connectedWires.length < 6) && (isRiddleSimple()) ) {
+            playBuzzer();
+        }
 
 
-	setInterval(function() {
-		pin.query(function (pinState) {
-			state.pinStates += pinState.value;
-		});
-	}, interval * 10);
+    }
 
-	setInterval(function() {
-		state.pinStates /= 10;
-		console.log("pin state is ", state.pinStates);
-	}, interval * 1000)
+
+
+    //
+    // Buttons actions - pushButton and switchButton
+    //
+    pushButton.on('press', function(){
+        pushButtonPress();
+        calculateAll();
+    });
+
+    pushButton.on('release', function(){
+        pushButtonRelease();
+        calculateAll();
+    });
+
+    pushButton.on('hold', function(){
+        pushButtonHold();
+        calculateAll();
+    });
+
+    switchButton.on('press', function(){
+        switchButtonPress();
+        calculateAll();
+    });
+
+    switchButton.on('release', function(){
+        switchButtonRelease();
+        calculateAll();
+    });
+
+
+    //
+    // Action from master desk
+    //
+    api.post('/set_start_simple', function (req, res) {
+        state.riddle_status = riddle_status.SIMPLE_START;
+        calculateAll();
+        res.json(readState());
+    });
+
+
 };
