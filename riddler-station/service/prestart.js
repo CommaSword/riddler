06fa8cba9d5c464661b2cce4091584a7c2ffe38d
DoronTsur <doront@wix.com>
2016-06-17 14:45:38 +0300

    make it as a service

---
+++ riddler-station/service/prestart.js
@@ -0,0 +1,17 @@
+var exec = require('child_process').execSync;
+try {
+  process.chdir('~/riddler/riddler-station');
+}
+catch (err) {
+  console.log('sorry no folder');
+  process.exit(1);
+}
+
+exec('git pull', function(error, stdout, stderr) {
+  if (error) {
+   	 console.log('could not pull running');
+  }
+  else {
+  	exec('npm i');
+  }
+});
