8d7012dae5f2b96a1afc39daf19f205f909b2090
amir-arad <amira@wix.com>
2016-06-22 00:26:03 +0300

    small fixes

--- riddler-hacker/src/components.js
+++ riddler-hacker/src/components.js
@@ -3,51 +3,48 @@ import React, {Component} from 'react';
 const stylesheet  = {
   layout: {
     width:'100%',
-    height:'1',
+    height:'1'
   }
-}
+};
 
 export class Welcome extends Component{
   static propTypes = {
-      done: React.PropTypes.func.isRequired
-  }
+    done: React.PropTypes.func.isRequired
+  };
   constructor(props) {
     super(props);
 
   }
 
   componentDidMount() {
-    this.refs.textbox1.focus();
+    this.refs.shipId.focus();
   }
 
-  handleKeypress1(ch){
+  handleKeypressShipId(ch){
     if(ch && ch.charCodeAt(0) == 13){
-      this.refs.textbox1.submit()
-      this.refs.textbox2.focus()
+      this.refs.shipId.submit();
+      this.refs.objective.focus();
     }
   }
-  handleKeypress2(ch){
-    if(ch.charCodeAt(0) == 13 && this.refs.textbox2.content != ''){
-      this.props.done(this.refs.textbox1.content, this.refs.textbox2.content);
+  handleKeypressOperation(ch){
+    if(ch.charCodeAt(0) == 13 && this.refs.objective.content != ''){
+      this.props.done(this.refs.shipId.content, this.refs.objective.content);
     }
   }
 
   render() {
     return(
-      <box
-        width={'100%'}
-        height={'100%'}
-        class={stylesheet.layout}>
+      <box width={'100%'} height={'100%'}>
         <text height={1} width={'100%'}>Welcome</text>
         <form>
           <box
             border={{type: 'line'}}
             style={{border: {fg: 'cyan'}}}
-            top={3} left={3} width={60} height={30}>
+            top={3} left={3} width={60} height={15}>
             <text top={2}>Ship ID:</text>
-            <textbox onKeypress={::this.handleKeypress1} inputOnFocus top={3} width={50} height={3} border={{type: 'line'}} ref='textbox1'></textbox>
-            <text top={6}>Hacking Attempt Request:</text>
-            <textbox onKeypress={::this.handleKeypress2} inputOnFocus top={7} width={50} height={3} border={{type: 'line'}} ref='textbox2'></textbox>
+            <textbox onKeypress={::this.handleKeypressShipId} inputOnFocus top={3} width={50} height={3} border={{type: 'line'}} ref='shipId'></textbox>
+            <text top={6}>Objective:</text>
+            <textbox onKeypress={::this.handleKeypressOperation} inputOnFocus top={7} width={50} height={3} border={{type: 'line'}} ref='objective'></textbox>
           </box>
         </form>
       </box>
