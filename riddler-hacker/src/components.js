de9e586a46d4721769e7b06f93ac7e3faf8fcf52
DoronTsur <doront@wix.com>
2016-06-22 00:57:38 +0300

    hacking game

--- riddler-hacker/src/components.js
+++ riddler-hacker/src/components.js
@@ -93,13 +93,41 @@ export class PreHack extends Component{
 }
 
 export class Hacking extends Component{
+  static propTypes = {
+    done: React.PropTypes.func.isRequired
+  }
   constructor(props) {
     super(props);
-
+    this.state = {first: Math.floor(Math.random() * 1000), second:Math.floor(Math.random() * 1000)};
 
   }
+  componentDidMount() {
+    this.refs.textbox.focus();
+  }
+  handleKeypress(ch) {
+    if(ch.charCodeAt(0) == 13){
+      let number = parseInt(this.refs.textbox.content)
+      number === this.state.frist * this.state.second && this.props.done();
+    }
+  }
   render() {
-    return(<text width={'100%'}>Hacking</text>)
+    return(
+      <box
+        width={'100%'}
+        height={'100%'}
+        class={stylesheet.layout}>
+        <text height={1} width={'100%'}>Calculate this:</text>
+        <form>
+          <box
+            border={{type: 'line'}}
+            style={{border: {fg: 'cyan'}}}
+            top={3} left={3} width={60} height={30}>
+            <text top={2}>{this.state.first  + ' * '  + this.state.second} </text>
+            <textbox onKeypress={::this.handleKeypress} inputOnFocus top={7} width={50} height={3} border={{type: 'line'}} ref='textbox'></textbox>
+          </box>
+        </form>
+      </box>
+      )
   }
 }
 
