2ab44be57809e4a0998bb32407696e02b36bcb8b
amir-arad <amira@wix.com>
2016-06-22 02:30:03 +0300

    fancy preHack

--- riddler-hacker/src/components.js
+++ riddler-hacker/src/components.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import recursiveReadSync from 'recursive-readdir-sync';
+import {code} from'./code-ipsum';
+
 
 const stylesheet  = {
   layout: {
@@ -35,57 +37,49 @@ export class Welcome extends Component{
 
   render() {
     return(
-      <box width={'100%'} height={'100%'}>
-        <text height={1} width={'100%'}>Welcome</text>
-        <form>
-          <box
-            border={{type: 'line'}}
-            style={{border: {fg: 'cyan'}}}
-            top={3} left={3} width={60} height={15}>
-            <text top={2}>Ship ID:</text>
-            <textbox onKeypress={::this.handleKeypressShipId} inputOnFocus top={3} width={50} height={3} border={{type: 'line'}} ref='shipId'></textbox>
-            <text top={6}>Objective:</text>
-            <textbox onKeypress={::this.handleKeypressOperation} inputOnFocus top={7} width={50} height={3} border={{type: 'line'}} ref='objective'></textbox>
-          </box>
-        </form>
-      </box>
-      )
+        <box width={'100%'} height={'100%'}>
+          <text height={1} width={'100%'}>Welcome</text>
+          <form>
+            <box
+                border={{type: 'line'}}
+                style={{border: {fg: 'cyan'}}}
+                top={3} left={3} width={60} height={15}>
+              <text top={2}>Ship ID:</text>
+              <textbox onKeypress={::this.handleKeypressShipId} inputOnFocus top={3} width={50} height={3} border={{type: 'line'}} ref='shipId'></textbox>
+              <text top={6}>Objective:</text>
+              <textbox onKeypress={::this.handleKeypressOperation} inputOnFocus top={7} width={50} height={3} border={{type: 'line'}} ref='objective'></textbox>
+            </box>
+          </form>
+        </box>
+    )
   }
 }
-
-let files = recursiveReadSync(process.cwd());
 
 export class PreHack extends Component{
   constructor(props) {
     super(props);
     this.state = {progress: 0};
-
-    const interval = setInterval(() => {
-      if (this.state.progress >= 100)
-        this.setState({progress: 0});
-
-      this.setState({progress: this.state.progress + 1});
-    }, 10);
   }
 
-  walkSync(dir) {
-    let fs = require('fs');
-    let filelist = [];
-    files.forEach((file) => {
-     if(this.state.progress > filelist.length) {
-        filelist.push(file);
-      }
-    });
-    return filelist;
+  componentDidMount(){
+    this.interval = setInterval(() => {
+      this.setState({progress: this.state.progress + 3});
+    }, 150);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.interval);
   }
 
   render() {
+    const pos = this.state.progress % code.length;
     return(
-      <box>
-        <text width={'100%'}>PreHack</text>
-        <list items={this.walkSync(process.cwd())}></list>
-      </box>
-      )
+        <box>
+          <box content={code.slice(pos, pos + 100).join('\n')} style={{fg: 'green'}}>
+            <text left="center" top="center" align="right" width="155">Processing{'...'.substring(0, (pos/10) % 4)}</text>
+          </box>
+        </box>
+    )
   }
 }
 
