bd2d49c82ae2ce47667cfa532073e1c4a44d4060
amir-arad <amira@wix.com>
2016-06-22 04:09:07 +0300

    fixed input bug

--- riddler-hacker/src/target.js
+++ riddler-hacker/src/target.js
@@ -25,8 +25,8 @@ export class Target extends Component{
 		}
 	}
 	handleKeypressOperation(ch, key) {
-		if(key.name === 'enter' && this.refs.objective.content != ''){
-			this.props.done(this.refs.shipId.content, this.refs.objective.content);
+		if(key.name === 'enter' && this.refs.objective.getValue() != ''){
+			this.props.done(this.refs.shipId.getValue(), this.refs.objective.getValue());
 		}
 	}
 
@@ -37,10 +37,9 @@ export class Target extends Component{
 				style={{border: {fg: 'cyan'}}}
 				top={3} left={3} width={60} height={15}>
 				<form>
-
-					<text top={2}>Ship ID:</text>
+					<text top={2}>ship sign</text>
 					<textbox onKeypress={::this.handleKeypressShipId} inputOnFocus top={3} width={50} height={3} border={{type: 'line'}} ref='shipId'></textbox>
-					<text top={6}>Objective:</text>
+					<text top={6}>objective</text>
 					<textbox onKeypress={::this.handleKeypressOperation} inputOnFocus top={7} width={50} height={3} border={{type: 'line'}} ref='objective'></textbox>
 				</form>
 			</box>
